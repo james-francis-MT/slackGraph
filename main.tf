b025b56c3cd9c40b9cc52129e9a9a5b12dac7089
James Francis <james.francis@madetech.com>
2023-05-24 17:11:18 +0100

    adding s3 bucket resource to tf

--- main.tf
+++ main.tf
@@ -1,17 +1,10 @@
 terraform {
-  cloud {
-    organization = "madetech-james-f"
-    workspaces {
-      name = "slack-grapher"
-    }
-  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 4.16"
     }
   }
-
   required_version = ">= 1.2.0"
 }
 
@@ -19,3 +12,6 @@ provider "aws" {
   region = "eu-west-2"
 }
 
+resource "aws_s3_bucket" "slack_data_store" {
+  bucket = "slack-data-store"
+}
